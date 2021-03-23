e05fdffcd16572350ae4ff9abe5d916f650ea516
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2021-03-23 20:10:42 +0000

    Added code to save datsets every Nframe timesteps.

--- UN-BN_vort.py
+++ UN-BN_vort.py
@@ -3,7 +3,6 @@ import cupy as cp
 import include.symplectic as sm
 import h5py
 
-
 # --------------------------------------------------------------------------------------------------------------------
 # Spatial and Potential parameters:
 # --------------------------------------------------------------------------------------------------------------------
@@ -39,7 +38,7 @@ c4 = -1000
 
 # Time steps, number and wavefunction save variables
 Nt = 1000
-Nframe = 100  # Saves data every Nframe time steps
+Nframe = 50  # Saves data every Nframe time steps
 dt = -1j * 1e-2  # Time step
 t = 0.
 
@@ -65,16 +64,56 @@ Psi = [psiP2, psiP1, psi0, psiM1, psiM2]  # Full 5x1 wavefunction
 alpha_angle = 0
 beta_angle = 0.1
 gamma_angle = 0
+
 Psi = sm.rotation(Psi, alpha_angle, beta_angle, gamma_angle)
 N = [dx * dy * cp.sum(cp.abs(wfn) ** 2) for wfn in Psi]  # Atom number of each component
 theta_fix = [cp.angle(wfn) for wfn in Psi]
 Psi = [cp.fft.fftn(wfn) for wfn in Psi]  # Transforming wfn to Fourier space
 
-# Coefficients for kinetic evolution:
-Ek = 0.5 * (Kx ** 2 + Ky ** 2 + Kz ** 2)
-A = cp.exp(-1j * (Ek + 4 * q) * dt / 2)
-B = cp.exp(-1j * (Ek + q) * dt / 2)
-C = cp.exp(-1j * Ek * dt / 2)
+# Store parameters in dictionary for saving
+parameters = {
+    "c0": c0,
+    "c2": c2,
+    "c4": c4,
+    "q": q,
+    "p": p,
+    "omega_trap": omega_trap,
+    "omega_rot": omega_rot,
+    "alpha, beta, gamma": [alpha_angle, beta_angle, gamma_angle]
+}
+
+# Create dataset and save initial state
+filename = 'UN-BN'  # Name of file to save data to
+data_path = 'data/{}.hdf5'.format(filename)
+k = 0  # Array index
+
+with h5py.File(data_path, 'w') as data:
+    # Saving spatial data:
+    data.create_dataset('grid/x', x.shape, data=cp.asnumpy(x))
+    data.create_dataset('grid/y', y.shape, data=cp.asnumpy(y))
+
+    # Saving time variables:
+    data.create_dataset('time/Nt', data=Nt)
+    data.create_dataset('time/dt', data=dt)
+    data.create_dataset('time/Nframe', data=Nframe)
+
+    # Save parameters:
+    data.create_dataset('parameters', data=str(parameters))
+
+    # Creating empty wavefunction datasets to store data:
+    data.create_dataset('wavefunction/psiP2', (Nx, Ny, Nz, 1), maxshape=(Nx, Ny, Nz, None), dtype='complex64')
+    data.create_dataset('wavefunction/psiP1', (Nx, Ny, Nz, 1), maxshape=(Nx, Ny, Nz, None), dtype='complex64')
+    data.create_dataset('wavefunction/psi0', (Nx, Ny, Nz, 1), maxshape=(Nx, Ny, Nz, None), dtype='complex64')
+    data.create_dataset('wavefunction/psiM1', (Nx, Ny, Nz, 1), maxshape=(Nx, Ny, Nz, None), dtype='complex64')
+    data.create_dataset('wavefunction/psiM2', (Nx, Ny, Nz, 1), maxshape=(Nx, Ny, Nz, None), dtype='complex64')
+
+    # Store initial state
+    data.create_dataset('initial_state/psiP2', data=cp.asnumpy(cp.fft.ifft2(Psi[0])))
+    data.create_dataset('initial_state/psiP1', data=cp.asnumpy(cp.fft.ifft2(Psi[1])))
+    data.create_dataset('initial_state/psi0', data=cp.asnumpy(cp.fft.ifft2(Psi[2])))
+    data.create_dataset('initial_state/psiM1', data=cp.asnumpy(cp.fft.ifft2(Psi[3])))
+    data.create_dataset('initial_state/psiM2', data=cp.asnumpy(cp.fft.ifft2(Psi[4])))
+
 
 # --------------------------------------------------------------------------------------------------------------------
 # Imaginary time:
@@ -96,9 +135,28 @@ for i in range(Nt):
     if i % 100 == 0:
         print('t = {}'.format(i * dt))
 
-with h5py.File('data/test_data.hdf5', 'w') as file:
-    file.create_dataset('wavefunction/psiP2', data=cp.asnumpy(cp.fft.ifftn(Psi[0])))
-    file.create_dataset('wavefunction/psiP1', data=cp.asnumpy(cp.fft.ifftn(Psi[1])))
-    file.create_dataset('wavefunction/psi0', data=cp.asnumpy(cp.fft.ifftn(Psi[2])))
-    file.create_dataset('wavefunction/psiM1', data=cp.asnumpy(cp.fft.ifftn(Psi[3])))
-    file.create_dataset('wavefunction/psiM2', data=cp.asnumpy(cp.fft.ifftn(Psi[4])))
+    # Saves data
+    if np.mod(i + 1, Nframe) == 0:
+        # Updates file with new wavefunction values:
+        with h5py.File(data_path, 'r+') as data:
+            new_psiP2 = data['wavefunction/psiP2']
+            new_psiP2.resize((Nx, Ny, Nz, k + 1))
+            new_psiP2[:, :, :, k] = cp.asnumpy(cp.fft.ifft2(Psi[0]))
+
+            new_psiP1 = data['wavefunction/psiP1']
+            new_psiP1.resize((Nx, Ny, Nz, k + 1))
+            new_psiP1[:, :, :, k] = cp.asnumpy(cp.fft.ifft2(Psi[1]))
+
+            new_psi0 = data['wavefunction/psi0']
+            new_psi0.resize((Nx, Ny, Nz, k + 1))
+            new_psi0[:, :, :, k] = cp.asnumpy(cp.fft.ifft2(Psi[2]))
+
+            new_psiM1 = data['wavefunction/psiM1']
+            new_psiM1.resize((Nx, Ny, Nz, k + 1))
+            new_psiM1[:, :, :, k] = cp.asnumpy(cp.fft.ifft2(Psi[3]))
+
+            new_psiM2 = data['wavefunction/psiM2']
+            new_psiM2.resize((Nx, Ny, Nz, k + 1))
+            new_psiM2[:, :, :, k] = cp.asnumpy(cp.fft.ifft2(Psi[4]))
+
+        k += 1  # Increment array index
