97f63a54479f6d40cd960fc6c179a4f3e263f6c2
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2021-03-24 19:02:26 +0000

    Added calc_a30 function for calculating the spin-singlet trio.

--- include/diagnostics.py
+++ include/diagnostics.py
@@ -33,7 +33,7 @@ def calc_spin_vectors(Wfn):
 
         return fp, fz
 
-    else:   # Single frame of data
+    else:  # Single frame of data
         psiP2, psiP1, psi0, psiM1, psiM2 = Wfn[0], Wfn[1], Wfn[2], Wfn[3], Wfn[4]
         fp = ne.evaluate("sqrt(6) * (psiP1 * conj(psi0) + psi0 * conj(psiM1)) "
                          "+ 2 * (psiM1 * conj(psiM2) + psiP2 * conj(psiP1))")
@@ -50,7 +50,7 @@ def calc_a20(psiP2, psiP1, psi0, psiM1, psiM2):
     :param psi0: psi_0 component
     :param psiM1: psi_-1 component
     :param psiM2: psi_-2 component
-    :return: a20, spin-singlet duo
+    :return: a20, spin-singlet pair
     """
 
     # Total density
@@ -59,3 +59,20 @@ def calc_a20(psiP2, psiP1, psi0, psiM1, psiM2):
     # Return a20
     return 1 / (np.sqrt(5) * n) * (2 * psiP2 * psiM2 - 2 * psiP1 * psiM1 + psi0 ** 2)
 
+
+def calc_a30(psiP2, psiP1, psi0, psiM1, psiM2):
+    """
+    :param psiP2: psi_+2 component
+    :param psiP1: psi_+1 component
+    :param psi0: psi_0 component
+    :param psiM1: psi_-1 component
+    :param psiM2: psi_-2 component
+    :return: a30, spin-singlet trio
+    """
+
+    # Total density
+    n = abs(psiP2) ** 2 + abs(psiP1) ** 2 + abs(psi0) ** 2 + abs(psiM1) ** 2 + abs(psiM2) ** 2
+
+    # Return a30
+    return 1 / (n * np.sqrt(n)) * (3 * np.sqrt(6) / 2 * (psiP1 ** 2 * psiM2 + psiM1 ** 2 * psiP2) +
+                                   psi0 * (psi0 ** 2 - 3 * psiP1 * psiM1 - 6 * psiP2 * psiM2))
