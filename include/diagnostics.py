e074477d8079d6960106c5756ef02b4f8cdec30c
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2021-03-26 17:01:16 +0000

    Changed density and normalise_wfn functions to now accept 5 element list of wavefunction components. Now calculate spin-singlet terms in terms of a normalised wavefunction.

--- include/diagnostics.py
+++ include/diagnostics.py
@@ -4,17 +4,32 @@ from numba import njit
 """File containing functions for calculating certain diagnostics accellerated using numba"""
 
 
-def calc_density(psiP2, psiP1, psi0, psiM1, psiM2):
+def calc_density(Wfn):
     """
-    :param psiP2: psi_+2 component
-    :param psiP1: psi_+1 component
-    :param psi0: psi_0 component
-    :param psiM1: psi_-1 component
-    :param psiM2: psi_-2 component
+    :param Wfn: List, each component is Nx x Ny x Nz wavefunction
     :return: n: total atomic density
     """
 
-    return abs(psiP2) ** 2 + abs(psiP1) ** 2 + abs(psi0) ** 2 + abs(psiM1) ** 2 + abs(psiM2) ** 2
+    return sum(abs(wfn) ** 2 for wfn in Wfn)
+
+
+def normalise_wfn(Wfn):
+    """
+    :param Wfn: List, each component is Nx x Ny x Nz wavefunction
+    :return: Normalised 5 x 1 wavefunction
+    """
+
+    # Density
+    n = calc_density(Wfn)
+
+    # Normalise wavefunction
+    Zeta = [wfn / np.sqrt(n) for wfn in Wfn]
+
+    # Correct division by small numbers outside of trap
+    for zeta in Zeta:
+        zeta[n < 1e-6] = 0
+
+    return Zeta
 
 
 @njit
@@ -35,54 +50,32 @@ def calc_spin_vectors(psiP2, psiP1, psi0, psiM1, psiM2):
     return fp, fz
 
 
-def normalise_wfn(psiP2, psiP1, psi0, psiM1, psiM2):
-    """
-    :param psiP2: psi_+2 component
-    :param psiP1: psi_+1 component
-    :param psi0: psi_0 component
-    :param psiM1: psi_-1 component
-    :param psiM2: psi_-2 component
-    :return: Normalised wavefunction components (same order as input)
-    """
-
-    # Density
-    n = calc_density(psiP2, psiP1, psi0, psiM1, psiM2)
-
-    return psiP2 / np.sqrt(n), psiP1 / np.sqrt(n), psi0 / np.sqrt(n), psiM1 / np.sqrt(n), psiM2 / np.sqrt(n)
-
-
 @njit
-def calc_spin_singlet_duo(psiP2, psiP1, psi0, psiM1, psiM2):
+def calc_spin_singlet_duo(zetaP2, zetaP1, zeta0, zetaM1, zetaM2):
     """
-    :param psiP2: psi_+2 component
-    :param psiP1: psi_+1 component
-    :param psi0: psi_0 component
-    :param psiM1: psi_-1 component
-    :param psiM2: psi_-2 component
+    :param zetaP2: normalised psi_+2 component
+    :param zetaP1: normalised psi_+1 component
+    :param zeta0: normalised psi_0 component
+    :param zetaM1: normalised psi_-1 component
+    :param zetaM2: normalised psi_-2 component
     :return: a20, spin-singlet duo
     """
 
-    # Total density
-    n = np.abs(psiP2) ** 2 + np.abs(psiP1) ** 2 + np.abs(psi0) ** 2 + np.abs(psiM1) ** 2 + np.abs(psiM2) ** 2
-
     # Return a20
-    return 1 / (np.sqrt(5) * n) * (2 * psiP2 * psiM2 - 2 * psiP1 * psiM1 + psi0 ** 2)
+    return 1 / (np.sqrt(5)) * (2 * zetaP2 * zetaM2 - 2 * zetaP1 * zetaM1 + zeta0 ** 2)
 
 
 @njit
-def calc_spin_singlet_trio(psiP2, psiP1, psi0, psiM1, psiM2):
+def calc_spin_singlet_trio(zetaP2, zetaP1, zeta0, zetaM1, zetaM2):
     """
-    :param psiP2: psi_+2 component
-    :param psiP1: psi_+1 component
-    :param psi0: psi_0 component
-    :param psiM1: psi_-1 component
-    :param psiM2: psi_-2 component
+    :param zetaP2: normalised psi_+2 component
+    :param zetaP1: normalised psi_+1 component
+    :param zeta0: normalised psi_0 component
+    :param zetaM1: normalised psi_-1 component
+    :param zetaM2: normalised psi_-2 component
     :return: a30, spin-singlet trio
     """
 
-    # Total density
-    n = np.abs(psiP2) ** 2 + np.abs(psiP1) ** 2 + np.abs(psi0) ** 2 + np.abs(psiM1) ** 2 + np.abs(psiM2) ** 2
-
     # Return a30
-    return 1 / (n * np.sqrt(n)) * (3 * np.sqrt(6) / 2 * (psiP1 ** 2 * psiM2 + psiM1 ** 2 * psiP2)
-                                   + psi0 * (psi0 ** 2 - 3 * psiP1 * psiM1 - 6 * psiP2 * psiM2))
+    return (3 * np.sqrt(6) / 2 * (zetaP1 ** 2 * zetaM2 + zetaM1 ** 2 * zetaP2)
+            + zeta0 * (zeta0 ** 2 - 3 * zetaP1 * zetaM1 - 6 * zetaP2 * zetaM2))
