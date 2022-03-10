c4aa30b8b1976a951052a087329b021aa6b4b547
Matt Wheeler <mattwheeler15@hotmail.co.uk>
2022-03-10 15:47:30 +0000

    Updated singlet-trio plot

--- plots/C-FM_singlets.py
+++ plots/C-FM_singlets.py
@@ -5,17 +5,10 @@ import matplotlib
 matplotlib.use('TkAgg')
 
 Nx = 1000
-eta_C_FM = np.zeros(Nx)
-eta_C_FM[:Nx // 3] = -1
-eta_C_FM[Nx // 3:2 * Nx // 3] = 0
-eta_C_FM[2 * Nx // 3:] = 2
-
-for i in range(100):
-    eta_C_FM[Nx // 3 + i] = -1 + i / 100
-    eta_C_FM[2 * Nx // 3 + i] = 0 + i / 50
+eta_C_FM = np.linspace(-1, 2, Nx)
 
 mod_a30_C_FM = 0.5 * (2 - eta_C_FM) ** 2 * (1 + eta_C_FM)
-plt.plot(eta_C_FM, mod_a30_C_FM, 'ko')
+plt.plot(eta_C_FM, mod_a30_C_FM, 'k')
 plt.ylabel(r'$|A_{30}|^2$')
 plt.xlabel(r'$\eta$')
 plt.show()
