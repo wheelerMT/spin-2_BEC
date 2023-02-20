6b668708e23359a0a145585604e59fac8f5cd007
Matt Wheeler <wheelerMT@pm.me>
2023-02-20 11:07:36 +0000

    Re-designed plot

--- plots/UN-BN_singlets.py
+++ plots/UN-BN_singlets.py
@@ -2,47 +2,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
-matplotlib.use('TkAgg')
-SMALL_SIZE = 6
-MEDIUM_SIZE = 8
-BIGGER_SIZE = 10
+plt.rcParams.update({"font.size": 14})
 
-plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
-plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
-plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+matplotlib.use("TkAgg")
 
 Nx = 1000
-eta_UN_BN = np.zeros(Nx)
-eta_UN_BN[:Nx // 2] = 1
-for i in range(100):
-    eta_UN_BN[Nx // 2 - i] = 0 + i / 100
-theta = np.linspace(0, np.pi, Nx)
-Eta, Theta = np.meshgrid(eta_UN_BN, theta, indexing='ij')
+eta_UN_BN = np.linspace(-1, 1, Nx)
+theta = np.linspace(0, 2 * np.pi, Nx)
+Eta, Theta = np.meshgrid(eta_UN_BN, theta, indexing="ij")
 
-mod_a30_UN_BN = Eta ** 2 * (Eta ** 4 - 6 * Eta ** 2 * (1 - Eta ** 2) * np.cos(2 * Theta)
-                            + 9 * (1 - Eta ** 2) ** 2)
-mod_a20_UN_BN = 0.2 * ((1 - Eta ** 2) ** 2 + 2 * Eta ** 2 * (1 - Eta ** 2) * np.cos(2 * Theta) + Eta ** 4)
+mod_a20_UN_BN = 0.1 * ((1 - Eta**2) * np.cos(Theta) + Eta**2 + 1)
+mod_a30_UN_BN = (
+    (1 + Eta)
+    / 4
+    * (3 * (Eta**2 - 1) * np.cos(Theta) + Eta * (5 * Eta - 8) + 5)
+)
 
-fig, ax = plt.subplots(1, 2, sharey=True, figsize=(3.54, 3.54), dpi=600)
-a30_plot = ax[1].contourf(Eta, Theta, mod_a30_UN_BN, levels=200, cmap='jet')
-ax[0].set_xlabel(r'$\eta$')
-ax[0].set_yticks([0, np.pi / 2, np.pi])
-ax[0].set_yticklabels(['0', r'$\frac{\pi}{2}$', r'$\pi$'])
-ax[0].set_ylabel(r'$\theta$')
-ax[0].set_title(r'$|a_{20}|^2$')
-ax[0].set_aspect(1.0 / ax[0].get_data_ratio(), adjustable='box')
-fig.colorbar(a30_plot, ax=ax[1], ticks=[0, 1, 2], fraction=0.046, pad=0.04)
 
-a20_plot = ax[0].contourf(Eta, Theta, mod_a20_UN_BN, levels=200, cmap='jet')
-ax[1].set_xlabel(r'$\eta$')
-ax[1].set_title(r'$|a_{30}|^2$')
-ax[1].set_aspect(1.0 / ax[1].get_data_ratio(), adjustable='box')
-fig.colorbar(a20_plot, ax=ax[0], ticks=[0, 1 / 5], fraction=0.046, pad=0.04)
+fig, ax = plt.subplots(1, 2, sharex=True, sharey=True)
+for axis in ax:
+    axis.set_box_aspect(1)
 
-plt.tight_layout()
-plt.savefig('../../plots/spin-2/a20-a30-varying.png', bbox_inches='tight')
+ax[0].set_xlabel(r"$\eta$")
+ax[1].set_xlabel(r"$\eta$")
+ax[0].set_ylabel(r"$\theta$")
+
+ax[0].set_yticks([0, np.pi, 2 * np.pi])
+ax[0].set_yticklabels([r"$0$", r"$\pi$", r"$2\pi$"])
+
+a20_plot = ax[0].contourf(Eta, Theta, mod_a20_UN_BN, levels=100, cmap="jet")
+a30_plot = ax[1].contourf(Eta, Theta, mod_a30_UN_BN, levels=100, cmap="jet")
+
+a20_cbar = fig.colorbar(
+    a20_plot, ax=ax[0], orientation="vertical", fraction=0.047, pad=0.02
+)
+a20_cbar.set_ticks([0, 0.2])
+a20_cbar.set_ticklabels(["0", "0.2"])
+
+
+a30_cbar = fig.colorbar(
+    a30_plot, ax=ax[1], orientation="vertical", fraction=0.047, pad=0.02
+)
+a30_cbar.set_ticks([0, 1, 2])
+a30_cbar.set_ticklabels(["0", "1", "2"])
+
+plt.savefig(
+    "../plots/spin-2/a20-a30-varying.png", dpi=200, bbox_inches="tight"
+)
 plt.show()
