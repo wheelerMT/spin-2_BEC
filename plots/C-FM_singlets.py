f850c99f6124aa04be6bc719b2ed77da7dcb8a13
Matt Wheeler <wheelerMT@pm.me>
2023-02-20 11:52:24 +0000

    Re-designed plot

--- plots/C-FM_singlets.py
+++ plots/C-FM_singlets.py
@@ -2,13 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
-matplotlib.use('TkAgg')
+plt.rcParams.update({"font.size": 16})
+matplotlib.use("TkAgg")
 
-Nx = 1000
-eta_C_FM = np.linspace(-1, 2, Nx)
+r = np.linspace(0, 6, 1000)
+eta_C_FM = 3 * np.tanh(r / 2) - 1
 
 mod_a30_C_FM = 0.5 * (2 - eta_C_FM) ** 2 * (1 + eta_C_FM)
-plt.plot(eta_C_FM, mod_a30_C_FM, 'k')
-plt.ylabel(r'$|A_{30}|^2$')
-plt.xlabel(r'$\eta$')
+psiP2 = np.sqrt((1 + eta_C_FM) / 3)
+psiM1 = np.sqrt((2 - eta_C_FM) / 3)
+
+mod_f = abs(2 * abs(psiP2) ** 2 - abs(psiM1) ** 2)
+fig, ax = plt.subplots(1, figsize=(6.4, 3))
+ax.set_xlim(0, 6)
+ax.set_ylim(0 - 0.1, 2.1)
+
+ax.plot(r, mod_f, "k", label=r"$|\langle\mathbf{\hat{F}}(\mathbf{r})\rangle|$")
+ax.plot(r, mod_a30_C_FM, "k--", label=r"$|A_{30}(\mathbf{r})|^2$")
+
+ax.set_xlabel(r"$r$")
+ax.set_ylabel("Value")
+ax.set_yticks([0, 1, 2])
+ax.set_yticklabels(["0", "1", "2"])
+ax.legend()
+
+plt.savefig(
+    "../plots/spin-2/spin_singlet_radius_analytical.pdf", bbox_inches="tight"
+)
 plt.show()
